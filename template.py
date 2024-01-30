f0050c141eec2dd261aadbeeaabc64252f7e0794
nowwnik <nik.butorin.2018@bk.ru>
2024-01-30 23:33:10 +0500

    добавил комменты

--- template.py
+++ template.py
@@ -4,17 +4,17 @@ import pygame
 
 clock = pygame.time.Clock()
 
-pygame.init() #инициализация pygame
-screen = pygame.display.set_mode((1280, 720)) #оперделение игрового окна втупую
+pygame.init()  # инициализация pygame
+screen = pygame.display.set_mode((1280, 720))  # оперделение игрового окна втупую
 # убрать рамку flags=pygame.NOFRAME
 
-pygame.display.set_caption("POOP MASTER") #название окна
-icon = pygame.image.load('images/1671710.jpg').convert() #игровая иконка
-pygame.display.set_icon(icon) #шоб иконка работала
+pygame.display.set_caption("POOP MASTER")  # название окна
+icon = pygame.image.load('images/1671710.jpg').convert()  # игровая иконка
+pygame.display.set_icon(icon)  # шоб иконка работала
 
-background = pygame.image.load('images/background1.jpg').convert_alpha() #задник
+background = pygame.image.load('images/background1.jpg').convert_alpha()  # задник
 
-#несколько какашек, чтоб интереснее было
+# несколько какашек, чтоб интереснее было
 poops = [
     pygame.image.load('images/poo/poo1.png').convert_alpha(),
     pygame.image.load('images/poo/poo2.png').convert_alpha(),
@@ -24,13 +24,13 @@ poops = [
     pygame.image.load('images/poo/poo6.png').convert_alpha(),
 ]
 
-#рандомно вытаскивает какашку
+# рандомно вытаскивает какашку
 poo = random.choice(poops)
 
-#хранит инфу о живых какашках
+# хранит инфу о живых какашках
 poop_list = []
 
-#голова чтобы думать, ноги чтобы ходить/ тут про ходить
+# голова чтобы думать, ноги чтобы ходить/ тут про ходить
 run_right = [
     pygame.image.load('images/right/right_1.png').convert_alpha(),
     pygame.image.load('images/right/right_2.png').convert_alpha(),
@@ -44,33 +44,33 @@ run_left = [
     pygame.image.load('images/left/left_4.png').convert_alpha(),
 ]
 
-#начальная установка игрового тела
+# начальная установка игрового тела
 player_speed = 10
 player_x = 100
 player_y = 500
 
-#прыжок, уааау
+# прыжок, уааау
 is_jump = False
 jump_count = 10
 
 player_animation_count = 0
 bg_x = 0
 
-#внутренний звук, но я его послал
+# внутренний звук, но я его послал
 background_sound = pygame.mixer.Sound('sounds/fon.mp3')
 # background_sound.play()
 
-#спавнер какашек
+# спавнер какашек
 poop_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(poop_timer, 3500)
 
-#типа блок шрифтов и надписей
+# типа блок шрифтов и надписей
 label = pygame.font.Font('fonts/Impact.ttf', 80)
 lose_label = label.render('ЛОХ!', False, (255, 0, 0))
 restar_label = label.render('не обосраться снова', False, (255, 100, 0))
 restar_label_rect = restar_label.get_rect(topleft=(420, 300))
 
-#патроны (туалетка)
+# патроны (туалетка)
 bullets_left = 5
 bullet = pygame.image.load('images/bulet.png').convert_alpha()
 bullets = []
@@ -80,22 +80,22 @@ gameplay = True
 # game loop
 running = True
 while running:
-    #отрисовка фона
+    # отрисовка фона
     screen.blit(background, (bg_x, 0))
     screen.blit(background, (bg_x + 1280, 0))
 
-    x = 0 #переменная для рисования туалетки
+    x = 0  # переменная для рисования туалетки
     if gameplay:
-        player_rect = run_left[0].get_rect(topleft=(player_x, player_y)) #чувствительный прямоугольник игрового тела
+        player_rect = run_left[0].get_rect(topleft=(player_x, player_y))  # чувствительный прямоугольник игрового тела
 
-        #отрисовка боезапаса
+        # отрисовка боезапаса
         if bullets_left > 0:
-            for i in range(0,bullets_left):
+            for i in range(0, bullets_left):
                 screen.blit(bullet, (x, 0))
                 x += 45
-                i+=1
+                i += 1
 
-        #движение какашек
+        # движение какашек
         if poop_list:
             for (i, element) in enumerate(poop_list):
                 screen.blit(poo, element)
@@ -104,25 +104,25 @@ while running:
                 if element.x < -10:
                     poop_list.pop(i)
 
-                #смерть игрвоого тела
+                # смерть игрвоого тела
                 if player_rect.colliderect(element):
                     gameplay = False
-        #читает нажатые клавиши
+        # читает нажатые клавиши
         keys = pygame.key.get_pressed()
 
-        #тело двигается
+        # тело двигается
         if keys[pygame.K_a]:
             screen.blit(run_left[player_animation_count], (player_x, player_y))
         else:
             screen.blit(run_right[player_animation_count], (player_x, player_y))
 
-        #тело передвигается
+        # тело передвигается
         if keys[pygame.K_a] and player_x > 30:
             player_x -= player_speed
         elif keys[pygame.K_d] and player_x < 700:
             player_x += player_speed
 
-        #это прыжок, тут реально какой-то старнный код
+        # это прыжок, тут реально какой-то старнный код
         if not is_jump:
             if keys[pygame.K_SPACE]:
                 is_jump = True
@@ -137,25 +137,24 @@ while running:
                 is_jump = False
                 jump_count = 10
 
-        #счётчик анимаций, чтоб они повторялись
+        # счётчик анимаций, чтоб они повторялись
         if player_animation_count == 3:
             player_animation_count = 0
         else:
             player_animation_count += 1
 
-        #движение фона, типа перс идёт
+        # движение фона, типа перс идёт
         bg_x -= 8
         if bg_x == -1280:
             bg_x = 0
 
-
-        #тело кидается туалеткой
+        # тело кидается туалеткой
         if pygame.mouse.get_pressed()[0] and bullets_left > 0:
             bullets.append(bullet.get_rect(topleft=(player_x + 30, player_y + 70)))
 
             bullets_left -= 1
 
-        #работа с туалеткой
+        # работа с туалеткой
         if bullets:
             for (i, element) in enumerate(bullets):
                 screen.blit(bullet, (element.x, element.y))
@@ -170,7 +169,7 @@ while running:
                             poop_list.pop(index)
                             bullets.pop(i)
 
-    #если тело умерло
+    # если тело умерло
     else:
         screen.fill((0, 0, 0))
         screen.blit(lose_label, (440, 150))
