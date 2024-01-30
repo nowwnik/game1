55deade12b684e3cfc294cee5cac7155172ff623
nowwnik <nik.butorin.2018@bk.ru>
2024-01-30 14:40:20 +0500

    пока какашка, но уже сформированная

--- template.py
+++ template.py
@@ -7,11 +7,24 @@ pygame.display.set_caption("My game MF")
 icon = pygame.image.load('images/1671710.jpg')
 pygame.display.set_icon(icon)
 
+square = pygame.Surface((50, 50))
+square.fill('Blue')
+
+font = pygame.font.Font("fonts/Impact.ttf", 35)
+text_surface = font.render('pidor', False, 'White')
+
+player = pygame.image.load('images/1671710.jpg')
+
 #game loop
 running = True
 while running:
 
-    #screen.fill((255, 0, 0))
+    screen.blit(square, (0, 0))
+
+    pygame.draw.circle(square, "Yellow", (45, 10), 5)
+
+    screen.blit(text_surface, (100,100))
+    screen.blit(player, (300,200))
 
     pygame.display.update()
 
@@ -19,8 +32,5 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_f:
-                screen.fill((255, 0, 0))
 
 
